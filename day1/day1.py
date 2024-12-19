48a8f8b4889ead1e837fd05ac7309a50c88d4263
pulo0 <bartexx13042005@gmail.com>
2024-12-19 21:06:07 +0100

    style: change variable names

--- day1/day1.py
+++ day1/day1.py
@@ -2,39 +2,39 @@
 # Source: https://adventofcode.com/2024/day/1
 # Date: 2024-12-01
 with open('input_day.txt') as file:
-    listInitial = []
-    listFirst = []
-    listSecond = []
+    list_initial = []
+    list_first = []
+    list_second = []
     for i in file.readlines():
         eachItem = i.split()
-        listInitial.append(eachItem)
-    for j in range(0, len(listInitial)):
-        listFirst.append(listInitial[j][0])
-        listSecond.append(listInitial[j][1])
-    sListFirst = sorted(listFirst)
-    sListSecond = sorted(listSecond)
+        list_initial.append(eachItem)
+    for j in range(0, len(list_initial)):
+        list_first.append(list_initial[j][0])
+        list_second.append(list_initial[j][1])
+    s_list_first = sorted(list_first)
+    s_list_second = sorted(list_second)
 
 
-def firstPart():
+def first_part():
     result = 0
-    for k in range(0, len(listInitial)):
-        divisionForFirst = int(sListFirst[k]) - int(sListSecond[k])
-        divisionForSecond = int(sListSecond[k]) - int(sListFirst[k])
-        if sListFirst[k] > sListSecond[k]:
-            result += divisionForFirst
+    for k in range(0, len(list_initial)):
+        division_for_first = int(s_list_first[k]) - int(s_list_second[k])
+        division_for_second = int(s_list_second[k]) - int(s_list_first[k])
+        if s_list_first[k] > s_list_second[k]:
+            result += division_for_first
         else:
-            result += divisionForSecond
+            result += division_for_second
     print(result)
 
 
-def secondPart():
+def second_part():
     result = 0
-    for k in range(0, len(listInitial)):
+    for k in range(0, len(list_initial)):
         multiply = 0
-        for l in range(0, len(listInitial)):
-            if sListFirst[k] == sListSecond[l]:
+        for l in range(0, len(list_initial)):
+            if s_list_first[k] == s_list_second[l]:
                 multiply += 1
-        result += int(sListFirst[k]) * multiply
+        result += int(s_list_first[k]) * multiply
     print(result)
 
 
@@ -43,8 +43,8 @@ choice = input('Select the answers of the first Advent Of Code day: \n'
                + 'Part 2 (Press number 2) \n')
 
 if choice == '1':
-    firstPart()
+    first_part()
 elif choice == '2':
-    secondPart()
+    second_part()
 else:
     print('Wrong choice')
